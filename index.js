0b3c35419634bd8e7dfe5e09a9da3dce682efe24
Rome Bell <romebell5@gmail.com>
2021-02-08 13:32:29 -0800

    draft 1: Add examples and forms with pictures:

---
+++ index.js
@@ -0,0 +1,63 @@
+// 1 - Environment
+require('dotenv').config();
+// 2 - Imports
+const express = require('express');
+const axios = require('axios');
+const ejsLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+
+// 3 - App Set up
+const app = express();
+app.set('view engine', 'ejs');
+
+// 4 - App Middleware
+app.use(express.urlencoded({ extended: false })); // req.body, req.params, req.query
+app.use(methodOverride('_method'));
+app.use(ejsLayouts);
+
+// 5 - Route (controllers)
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/form1', (req, res) => {
+    res.render('form1');
+});
+
+app.get('/github', (req, res) => {
+    axios.get('https://api.github.com/users/romebell')
+    .then(response => {
+        console.log(response.data); // What datatype is this? (object)
+        res.render('github', { data: response.data });
+    });
+});
+
+app.get('/example1/:id', (req, res) => {
+    // print req.parmams in the terminal
+    console.log(req.params);
+    // print the id
+    console.log(req.params.id);
+    res.render('example1', { paramsObject: req.params });
+});
+
+ap.get('result1', (req, res) => {
+    res.render('result1');
+});
+
+// POST ROUTES
+app.post('/form1', (req, res) => {
+    const incomingData = req.body; // what datatype is this? (object)
+    console.log(incomingData); // print incomingData
+    res.redirect('result1', { data: incomingData }); // send data to view
+});
+
+
+// Set up PORT and listen on PORT
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
